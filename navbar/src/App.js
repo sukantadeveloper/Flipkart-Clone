dfc6ac04c9444e33a3c58f61daa13fa03ff24179
Vikas Yadav <vikasyadav3935@gmail.com>
2022-10-12 03:14:38 +0530

    navbar

---
+++ navbar/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
