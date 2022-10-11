dcdca0f00cb49804a272cbe450b225530fd9b6d0
gauravv8090 <gaurav8090983461@gmail.com>
2022-10-11 18:23:27 +0530

    cart page half

---
+++ cart-page/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Box, Heading } from '@chakra-ui/react';
+import CartPage from './Components/CartPage';
+
+function App() {
+  return (
+      <Box className="App" >
+        <CartPage/>
+      </Box>
+  );
+}
+
+export default App;
