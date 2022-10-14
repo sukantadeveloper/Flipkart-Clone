3179636e1e76160309b185a290a87ec7e42735c1
Rajgupta7080 <rajgupta09875@gmail.com>
2022-10-15 02:53:58 +0530

    login-signup

--- login/src/Components/Login.jsx
+++ login/src/Components/Login.jsx
@@ -1,172 +1,332 @@
-import React from "react";
-import { Signup } from "./SignUp";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Signup } from './SignUp'
+import { useState, useEffect } from 'react'
+import { Link, Navigate } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Box,
+  Text,
+  Image,
+  FormControl,
+  Input,
+  FormLabel,
 } from '@chakra-ui/react'
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    useDisclosure,
-    Box,
-    Text,
-    Image,
-    FormControl,
-    Input,
-    FormLabel,
-} from '@chakra-ui/react'
-
 
 export function Login() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const initialRef = React.useRef(null)
-    const finalRef = React.useRef(null)
+  const myotp = Math.floor(Math.random() * 999999) + 100000
 
-    const initialvalues = {email:"", password :""};
-    const[inputValues, setInputValues] = useState(initialvalues);
-    const[error, setError] = useState({});
-    const[isSubmit, setIsSubmit] = useState(false);
-    const [isAuth, setIsAuth] = useState(false);
-    const [isCheck, setIscheck] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = React.useRef(null)
+  const finalRef = React.useRef(null)
 
-
-
-
-
-
-
-    const handleChange =(inp)=>{
-      const {name,value} = inp.target;
-      setInputValues({...inputValues, [name] : value});
-      console.log(inputValues);
-    }
-    
-    const handlelogin = (inputValues) => {
-      fetch(`http://localhost:4000/Userdetails`)
-        .then((res) => res.json()).then((res)=>{
-          res.map((el)=>{
-            if(el.email==inputValues.email && el.password==inputValues.password){
-              if(inputValues){
-                setIsAuth(true);
-              }
-             
-            }
-          
-          })
-        })
-    };
-  
-  
-    useEffect(()=>{
-     
-      handlelogin(inputValues);
-      if(Object.keys(error).length===0 && isSubmit){
-         
-
-      }
-
-    },[error])
-
-
-    const handleSubmit =(e)=>{
-        e.preventDefault();
-        setError(validate(inputValues));
-        setIsSubmit(true);
-    
-    }
-
-    const validate =(values)=>{
-      const errors = {};
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-      if(values.email==""){
-        errors.email = "email is required";
-      }
-      else if(!regex.test(values.email)){
-        errors.email = "Enter a valid email";
-      }
-      if(values.password==""){
-        errors.password = "password is required";
-      }
-      else if(values.password.length<6){
-        errors.password = "password must not be less than 6 character";
-      }
-      return errors;
-    }
-
-    return (
-      <>
-
-
-
-        <Button bg="white"  textAlign="center" onClick={onOpen}>login</Button>
-        <Modal
-          loginRef={initialRef}
-          finalFocusRef={finalRef}
-          isOpen={isOpen}
-          onClose={onClose}
-          size = "2xl"
-          padding = "0px"
-        >
-          <ModalOverlay />
-          <ModalCloseButton />
-     
-          <ModalContent>
-           
-            <ModalBody  padding="-1.5" >
-
-            <div style={{display:"flex"}}>
-               <Box height="32rem" bg="#2874f0" width="16rem" padding="35px">
-                <Text fontWeight="500" color="white" fontSize='3xl'>Login</Text>
-                 <Text fontWeight="500"  marginTop="15px" color="#Dbdbdb" fontSize='1xl'>Get access to your <br /> Orders, Wishlist and Recommendations</Text>
-                 <Image marginTop="10rem" src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/login_img_c4a81e.png' aend='image' />
-               </Box>
-               <Box  height="32rem"  padding="35" width="24rem" color="#878787">
-                   
-               <FormControl>
-              {
-                (isAuth ? (<Alert width={150} marginBottom={2}  height={6} status='success'>
-                <AlertIcon height={4} />
-                <AlertTitle>Logged in</AlertTitle>
-              </Alert>) :  ""
-              )}
-              
-               
-                <FormLabel>Email address</FormLabel>
-               <Input color="black" marginTop="-3" name="email"  variant="flushed"  placeholder="Enter Email"  value={inputValues.email} onChange={handleChange}  />
-               <Text color="red" fontSize='xs'>{error.email}</Text>
-               <FormLabel marginTop="5">Password</FormLabel>
-              <Input color="black" marginTop="-3" name="password" type="password"  variant="flushed" placeholder="Enter Password" value={inputValues.password} onChange={handleChange} />
-              <Text  color="red" fontSize='xs'>{error.password}</Text>
-              <Text marginTop="5" fontSize='xs'>By continuing, you agree to Flipkart's <Link color="#2f74f0" href="">Terms of Use </Link>and <Link color="#2f74f0" href="">Privacy Policy.</Link></Text>
-              
-              <Button onClick={handleSubmit} borderRadius="0.5"  marginTop="4" padding="6" color="white" bg="#fb641b" width="19.7rem">Login</Button>
-              <Text marginTop="2" marginBottom="2" textAlign="center">OR</Text>
-              <Button boxShadow='md' p='6' rounded='md' borderRadius="0.5" padding="6" color="#2f74f0" bg="white" width="19.7rem">Request OTP</Button>
-              <Link><Text marginTop="2" textAlign="center" color="#2f74f0">New to Flipkart? {<Signup/>} </Text> </Link>
-              </FormControl>
-
-               </Box>
-                </div> 
-
-           
-           
-            </ModalBody>
-  
-           
-          </ModalContent>
-        </Modal>
-
-
-      </>
-    )
+  const initialvalues = { email: '', password: '' }
+  const [inputValues, setInputValues] = useState(initialvalues)
+  const [error, setError] = useState({})
+  const [isSubmit, setIsSubmit] = useState(false)
+  const [isAuth, setIsAuth] = useState(0)
+  const [isCheck, setIsCheck] = useState(false)
+  const [incorrect, setIncorrect] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  let checkOtp = (e) => {
+    setIsAuth(e.target.value)
   }
 
+  const handleChange = (inp) => {
+    const { name, value } = inp.target
+    setInputValues({ ...inputValues, [name]: value })
+    // console.log(inputValues)
+  }
 
+  const handlelogin = (inputValues) => {
+    fetch(`http://localhost:4000/Userdetails`)
+      .then((res) => res.json())
+      .then((res) => {
+        res.filter((el) => {
+          return el.email == inputValues.email &&
+            el.password == inputValues.password
+            ? setCorrect(true)
+            : setIncorrect(true)
+        })
+        // res.map((el) => {
+        //   // setIncorrect(false)
+        //   // console.log(inputValues, 'hhhhhhhhhhh')
+        //   if (
+        //     el.email == inputValues.email &&
+        //     (el.password == inputValues.password || isAuth == myotp)
+        //   ) {
+        //     // notify()
+        //     setCorrect(1)
+        //     console.log(correct)
+        //     // setIncorrect(false)
+
+        //   }
+        //   //  else if (
+        //   //   (el.email == inputValues.email &&
+        //   //     el.password != inputValues.password) ||
+        //   //   (el.email != inputValues.email &&
+        //   //     el.password == inputValues.password)
+        //   // ) {
+        //   //   setIncorrect(true)
+        //   //   // setCorrect(false)
+        //   //   return
+        //   // }
+        //   else {
+        //     setIncorrect(incorrect + 1)
+        //     console.log(incorrect)
+        //   }
+        // })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const notify = () => {
+    setCorrect(false)
+    setIncorrect(false)
+    toast('login SuccesFully', {
+      position: 'top-center',
+    })
+  }
+
+  const check = () => {
+    toast('Incorrect Details', {
+      position: 'top-center',
+    })
+    setIncorrect(false)
+  }
+  if (correct == true) {
+    // setIncorrect(0)
+    notify()
+  } else if (incorrect == true) {
+    // console.log('ra')
+    check()
+  }
+  // if (incorrect == 1) {
+  //   check()
+  // } else {
+  //   console.log('raj')
+  // }
+  useEffect(() => {
+    if (Object.keys(error).length === 0 && isSubmit) {
+    }
+  }, [error])
+
+  const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    if (values.email == '') {
+      errors.email = 'email is required'
+    } else if (!regex.test(values.email)) {
+      errors.email = 'Enter a valid email'
+    }
+    if (values.password == '') {
+      errors.password = 'password is required'
+    } else if (values.password.length < 6) {
+      errors.password = 'password must not be less than 6 character'
+    }
+    return errors
+  }
+  const getOtp = () => {
+    toast(myotp, {
+      position: 'top-center',
+    })
+  }
+
+  const Otp = () => {
+    setIsCheck(true)
+    getOtp()
+  }
+  const Pass = () => {
+    setIsCheck(false)
+  }
+
+  const handleSubmit = (e) => {
+    handlelogin(inputValues)
+    e.preventDefault()
+    setError(validate(inputValues))
+    setIsSubmit(true)
+    setInputValues(initialvalues)
+  }
+
+  return (
+    <>
+      <Button bg="white" textAlign="center" onClick={onOpen}>
+        login
+      </Button>
+      <Modal
+        loginRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        size="2xl"
+        padding="0px"
+      >
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalBody padding="-1.5">
+            <ToastContainer />
+            <ModalCloseButton
+              size="lg"
+              color="white"
+              marginRight="-3.5rem"
+              marginTop="-4"
+            />
+            <div style={{ display: 'flex' }}>
+              <Box height="32rem" bg="#2874f0" width="16rem" padding="35px">
+                <Text fontWeight="500" color="white" fontSize="3xl">
+                  Login
+                </Text>
+                <Text
+                  fontWeight="500"
+                  marginTop="15px"
+                  color="#Dbdbdb"
+                  fontSize="1xl"
+                >
+                  Get access to your <br /> Orders, Wishlist and Recommendations
+                </Text>
+                <Image
+                  marginTop="10rem"
+                  src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/login_img_c4a81e.png"
+                  aend="image"
+                />
+              </Box>
+              <Box height="32rem" padding="35" width="24rem" color="#878787">
+                <FormControl>
+                  <FormLabel>Email address</FormLabel>
+                  <Input
+                    color="black"
+                    marginTop="-3"
+                    name="email"
+                    variant="flushed"
+                    placeholder="Enter Email"
+                    value={inputValues.email}
+                    onChange={handleChange}
+                  />
+                  <Text color="red" fontSize="xs">
+                    {error.email}
+                  </Text>
+
+                  {isCheck ? (
+                    <>
+                      {' '}
+                      <FormLabel marginTop="5">Enter your OTP</FormLabel>
+                      <Input
+                        color="black"
+                        marginTop="-3"
+                        name="password"
+                        type="number"
+                        variant="flushed"
+                        placeholder="Enter OTP"
+                        value={isAuth}
+                        onChange={checkOtp}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      {' '}
+                      <FormLabel marginTop="5">Password</FormLabel>
+                      <Input
+                        color="black"
+                        marginTop="-3"
+                        name="password"
+                        type="password"
+                        variant="flushed"
+                        placeholder="Enter Password"
+                        value={inputValues.password}
+                        onChange={handleChange}
+                      />
+                      <Text color="red" fontSize="xs">
+                        {error.password}
+                      </Text>
+                    </>
+                  )}
+
+                  <Text marginTop="5" fontSize="xs">
+                    By continuing, you agree to Flipkart's{' '}
+                    <Link color="#2f74f0" href="">
+                      Terms of Use{' '}
+                    </Link>
+                    and{' '}
+                    <Link color="#2f74f0" href="">
+                      Privacy Policy.
+                    </Link>
+                  </Text>
+
+                  <Button
+                    onClick={handleSubmit}
+                    borderRadius="0.5"
+                    marginTop="4"
+                    padding="6"
+                    color="white"
+                    bg="#fb641b"
+                    width="19.7rem"
+                  >
+                    Login
+                  </Button>
+                  <Text marginTop="2" marginBottom="2" textAlign="center">
+                    OR
+                  </Text>
+                  {!isCheck ? (
+                    <>
+                      {' '}
+                      <Button
+                        onClick={Otp}
+                        boxShadow="md"
+                        p="6"
+                        rounded="md"
+                        borderRadius="0.5"
+                        padding="6"
+                        color="#2f74f0"
+                        bg="white"
+                        width="19.7rem"
+                      >
+                        Request OTP
+                      </Button>
+                    </>
+                  ) : (
+                    <>
+                      {' '}
+                      <Button
+                        onClick={Pass}
+                        boxShadow="md"
+                        p="6"
+                        rounded="md"
+                        borderRadius="0.5"
+                        padding="6"
+                        color="#2f74f0"
+                        bg="white"
+                        width="19.7rem"
+                      >
+                        Request Password
+                      </Button>
+                    </>
+                  )}
+                  <Link>
+                    <Text
+                      marginTop="2"
+                      bg="white"
+                      textAlign="center"
+                      color="#2f74f0"
+                    >
+                      New to Flipkart? {<Signup />}{' '}
+                    </Text>{' '}
+                  </Link>
+                </FormControl>
+              </Box>
+            </div>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
--- login/src/Components/SignUp.jsx
+++ login/src/Components/SignUp.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Login } from "./Login";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
   Alert,
   AlertIcon,
   AlertTitle,
-  AlertDescription,
+  position
 } from '@chakra-ui/react'
 import {
     Modal,
@@ -25,102 +27,123 @@ import {
 } from '@chakra-ui/react'
 export function Signup() {
     const { isOpen, onOpen, onClose } = useDisclosure()
-    const [isAuth, setIsAuth] = useState(false);
     const initialRef = React.useRef(null)
     const finalRef = React.useRef(null)
     
   
-    
+   
     const initialvalues = {email:"", password :""};
     const[inputValues, setInputValues] = useState(initialvalues);
     const[error, setError] = useState({});
+    const [isAuth, setIsAuth] = useState(false);
     const[isSubmit, setIsSubmit] = useState(false);
-    const[isCheck, setIscheck] = useState(false);
+
+  var flag = false;
     
  
     const handleChange =(inp)=>{
       const {name,value} = inp.target;
       setInputValues({...inputValues, [name] : value});
-      console.log(inputValues);
+      // console.log(inputValues);
     }
 
     const handleSignup = (body) => {
-
-     
-        fetch(`http://localhost:4000/Userdetails`)
-          .then((res) => res.json()).then((res)=>{
-            res.map((el)=>{
-              if(el.email==body.email){
-             setIscheck(true);
-          }
-            })
+       fetch(`http://localhost:4000/Userdetails`)
+        .then((res) => res.json()).then((res)=>{
+          res.map((el)=>{
+            if(el.email==body.email){
+              flag = true;
+        }
           })
-
-      if(isCheck){
-        fetch(`http://localhost:4000/Userdetails`, {
-          method: "POST",
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json"
+        }).then(()=>{
+          if(flag == false){
+            console.log(inputValues + " ");
+            console.log(flag + "raj")
+            fetch(`http://localhost:4000/Userdetails`, {
+              method: "POST",
+              body: JSON.stringify(body),
+              headers: {
+                "Content-Type": "application/json"
+              }
+            })
+              .then(() =>{ setIsAuth(true)
+                notify();
+              }
+              )
+      
+              .catch(() => setIsAuth(false));
+          }else{
+            check();
           }
         })
-          .then(() => {setIsAuth(true)
-            return setIscheck(false);
-               }
-          )
+ 
 
-          .catch(() => setIsAuth(false));
-      };
-      }
 
-    
+  
+    }
+
+  
 
 
    
 
-  
+      const notify =()=>{
+        toast("SignUp SuccesFully",{
+        position:"top-center"})
+
+      };
+      
+
+
+      const check=()=>{
+        toast.error("Already Exist",{
+          position:"top-center"
+        })
+        
+      };
+
+      const validate =(values)=>{
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if(values.email==""){
+          errors.email = "email is required";
+        }
+        else if(!regex.test(values.email)){
+          errors.email = "Enter a valid email";
+        }
+        if(values.password==""){
+          errors.password = "password is required";
+        }
+        else if(values.password.length<6){
+          errors.password = "password must not be less than 6 character";
+        }
+        return errors;
+      }
 
     useEffect(()=>{
       if(Object.keys(error).length===0 && isSubmit){
-           handleSignup(inputValues);
-
+        
       }
      
 
     },[error])
 
-   
 
     const handleSubmit =(e)=>{
         e.preventDefault();
         setError(validate(inputValues));
+        handleSignup(inputValues)
         setIsSubmit(true);
-        setIsAuth(false)
-     
+        setInputValues(initialvalues)
     }
 
-    const validate =(values)=>{
-      const errors = {};
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-      if(values.email==""){
-        errors.email = "email is required";
-      }
-      else if(!regex.test(values.email)){
-        errors.email = "Enter a valid email";
-      }
-      if(values.password==""){
-        errors.password = "password is required";
-      }
-      else if(values.password.length<6){
-        errors.password = "password must not be less than 6 character";
-      }
-      return errors;
-    }
   
+  
+
 
     return (
       <>
-        <Button fontWeight="400" marginBottom="2" bg="white" textAlign="center" onClick={onOpen}>Signup</Button>
+        <Button  _hover={{ bg: 'white' }} fontWeight="400" marginBottom="2" bg="white" textAlign="center" onClick={onOpen}>Signup</Button>
   
         <Modal
           SignupRef={initialRef}
@@ -130,40 +153,26 @@ export function Signup() {
           size = "2xl"
           padding = "0px"
         >
+
           <ModalOverlay />
         
           <ModalContent>
            
             <ModalBody  padding="-1.5" >
+            <ToastContainer/>
+            <ModalCloseButton size="lg" color="white" marginRight="-3.5rem" marginTop="-4" />
             <div style={{display:"flex"}}>
                <Box height="32rem" bg="#2874f0" width="16rem" padding="35px">
                 <Text fontWeight="600" color="white" fontSize='2xl'>Looks like you're new here!</Text>
-                 <Text fontWeight="600"  marginTop="15px" color="#Dbdbdb" fontSize='1xl'>Sign up with your mobile <br /> number to get started</Text>
+                 <Text fontWeight="600"  marginTop="15px" color="#Dbdbdb" fontSize='1xl'>Sign up with your email<br /> address to get started</Text>
                  <Image marginTop="10rem" src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/login_img_c4a81e.png' alt='image' />
                </Box>
                <Box  height="32rem"  padding="35" width="24rem" color="#878787">
                    
                <FormControl>
-               {
-                (isCheck ? (<Alert width={250} marginBottom={2}  height={6} status='error'>
-                <AlertIcon height={4} />
-                <AlertTitle>Already exist</AlertTitle>
-              </Alert>) : (isAuth ? (<Alert width={250} marginBottom={2}  height={6} status='success'>
-                <AlertIcon height={4} />
-                <AlertTitle>SignUp Succesfully</AlertTitle>
-              </Alert>) : ""
-     
-              )
-     
-              )}
+             
+        
             
-               {/* {
-                (isAuth ? (<Alert width={250} marginBottom={2}  height={6} status='success'>
-                <AlertIcon height={4} />
-                <AlertTitle>SignUp Succesfully</AlertTitle>
-              </Alert>) : ""
-     
-              )} */}
             
                <FormLabel>Email address</FormLabel>
                <Input color="black" marginTop="-3" name="email"  variant="flushed"  placeholder="Enter Email"  value={inputValues.email} onChange={handleChange}  />
