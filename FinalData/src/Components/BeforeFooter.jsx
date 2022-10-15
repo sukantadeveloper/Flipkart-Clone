7cf4643632678ebdddb9131238c83332eed62227
sukantad <ghoshsu4@gmail.com>
2022-10-15 10:43:15 +0530

    try to add more perfection

--- FinalData/src/Components/BeforeFooter.jsx
+++ FinalData/src/Components/BeforeFooter.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function BeforeFooter() {
     return (
         <div> <hr />
-            <Box p="25px" bg="#FFFFFF" color={"#757171"} borderTop="2px solid silver">
+            <Box p="25px" bg="#FFFFFF" color={"#757171"} borderTop="2px solid silver" display={{base:"none", md:"block", lg:"block"}}>
                 <Text fontSize={"17px"} pb="9px" fontWeight={"bold"}>    Flipkart: The One-stop Shopping Destination</Text>
                 <Text fontSize={"12px"} pb="9px">   E-commerce is revolutionizing the way we all shop in India. Why do you want to hop from one store to another in search of the latest phone when you can find it on the Internet in a single click? Not only mobiles. Flipkart houses everything you can possibly imagine, from trending electronics like laptops, tablets, smartphones, and mobile accessories to in-vogue fashion staples like shoes, clothing and lifestyle accessories; from modern furniture like sofa sets, dining tables, and wardrobes to appliances that make your life easy like washing machines, TVs, ACs, mixer grinder juicers and other time-saving kitchen and small appliances; from home furnishings like cushion covers, mattresses and bedsheets to toys and musical instruments, we got them all covered. You name it, and you can stay assured about finding them all here. For those of you with erratic working hours, Flipkart is your best bet. Shop in your PJs, at night or in the wee hours of the morning. This e-commerce never shuts down.
                 </Text>
